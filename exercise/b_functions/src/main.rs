e27c744516a58b16e3f6e057fc0633f7fec7614e
Nicolás César Sabbatini Vrech <nik.code.things@gmail.com>
2022-10-25 00:30:50 -0300

    Exercise B

--- exercise/b_functions/src/main.rs
+++ exercise/b_functions/src/main.rs
@@ -6,12 +6,15 @@ fn main() {
     let height = 7;
     let depth = 10;
     // 1. Try running this code with `cargo run` and take a look at the error.
-    //
+    /*
+      --> src/main.rs:15:28
+       |
+    15 |     println!("Area is {}", area);
+       |                            ^^^^ not found in this scope
+    */
     // See if you can fix the error. It is right around here, somewhere.  If you succeed, then
     // doing `cargo run` should succeed and print something out.
-    {
-        let area = area_of(width, height);
-    }
+    let area = area_of(width, height);
     println!("Area is {}", area);
 
     // 2. The area that was calculated is not correct! Go fix the area_of() function below, then run
@@ -25,16 +28,20 @@ fn main() {
     //
     // If you get stuck, remember that this is *very* similar to what `area_of` does.
     //
-    //println!("Volume is {}", volume(width, height, depth));
+    println!("Volume is {}", volume(width, height, depth));
 }
 
 fn area_of(x: i32, y: i32) -> i32 {
     // 2a. Fix this function to correctly compute the area of a rectangle given
     // dimensions x and y by multiplying x and y and returning the result.
     //
-    return 0;
     // Challenge: It isn't idiomatic (the normal way a Rust programmer would do things) to use
     //            `return` on the last line of a function. Change the last line to be a
     //            "tail expression" that returns a value without using `return`.
     //            Hint: `cargo clippy` will warn you about this exact thing.
+    x * y
+}
+
+fn volume(x: i32, y: i32, z: i32) -> i32 {
+    x * y * z
 }
